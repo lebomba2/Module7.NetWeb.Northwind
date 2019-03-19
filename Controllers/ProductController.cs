d5f8f8a07e1280747fddf20669384dd47de417e3
lebomba2 <lebomba2@gmail.com>
2019-03-19 14:18:12 -0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        // pass the Categories table to view, ordered by CategoryName
+        public IActionResult Category() => View(repository.Categories.OrderBy(c => c.CategoryName));
+        // pass the Products table to view, where CategoryId is equal to the id passed in, and the product is not discounted
+        public IActionResult CategoryDetail(int id) => View(repository.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+        // pass the discounts table to view, where the time of the discount is valid
+        public IActionResult Discounts() => View(repository.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
+    }
+}
